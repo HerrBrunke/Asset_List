0a7f94561cb31193fd248ab7c9fd3c2cb682a7a3
HerrBrunke <34299221+HerrBrunke@users.noreply.github.com>
2018-11-23 13:19:10 +0100

    First 18 Minutes
    lel. kek

---
+++ Assets/UI/Compass/Scripts/TestPlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerController : MonoBehaviour {
+	 public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		Player.transform.position = new Vector3(1, 1, 1);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("w"))
+		{
+			
+		}
+	}
+}
